4c91bfe5a80ea9ba542ce4d3de42e8f1a1041a36
Simran Chaudhari <56070375+chaudharisimran@users.noreply.github.com>
2024-07-12 23:24:45 +0530

    Update Maximum Score from removing substring.py

--- String/Maximum Score from removing substring.py
+++ String/Maximum Score from removing substring.py
@@ -1,1 +1,63 @@
+"""
+Problem Statement: You are given a string s and two integers x and y. You can perform two types of operations any number of times.
 
+Remove substring "ab" and gain x points.
+For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+Remove substring "ba" and gain y points.
+For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+Return the maximum points you can gain after applying the above operations on s.
+
+ 
+
+Example 1:
+
+Input: s = "cdbcbbaaabab", x = 4, y = 5
+Output: 19
+Explanation:
+- Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+- Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+- Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+- Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+Total score = 5 + 4 + 5 + 5 = 19.
+Example 2:
+
+Input: s = "aabbaaxybbaabb", x = 5, y = 4
+Output: 20
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+1 <= x, y <= 104
+s consists of lowercase English letters.
+"""
+
+#Solution 1
+def maximumGain(self, s, x, y):
+        """
+        :type s: str
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        def remove_pairs(s, pair, score):
+            res = 0
+            stack = []
+            for c in s:
+                if c == pair[1] and stack and stack[-1] == pair[0]:
+                    stack.pop()
+                    res += score
+                else:
+                    stack.append(c)
+            return "".join(stack), res
+        
+        res = 0
+        pair = "ab" if x > y else "ba"
+        modified_s, score = remove_pairs(s, pair, max(x, y))
+        res += score
+        
+        modified_s, score = remove_pairs(modified_s, pair[::-1], min(x, y))
+        res += score
+        
+        return res
+        
